c261970d0578241619d87f4bec1eb592220a3e39
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-05-15 14:36:16 +0530

    fix:build error

---
+++ components/mycollabs/groups.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const groups = () => {
+  return <div>groups</div>;
+};
---
+++ components/mycollabs/request.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const request = () => {
+  return <div>request</div>;
+};
